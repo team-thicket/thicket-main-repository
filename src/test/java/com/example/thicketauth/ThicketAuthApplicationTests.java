9adf31c42f0a1c1255109d0fc3194a96f7e31997
oh jinseok <ojs258@naver.com>
2023-11-10 15:20:26 +0900

    init

---
+++ src/test/java/com/example/thicketauth/ThicketAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thicketauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThicketAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
