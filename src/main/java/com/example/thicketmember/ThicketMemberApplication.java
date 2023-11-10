84f7baf5699073206ce48e4323b98e908c25df8c
oh jinseok <ojs258@naver.com>
2023-11-10 14:35:30 +0900

    2023-11-10 프로젝트-생성 멤버-레이어-초기설정

---
+++ src/main/java/com/example/thicketmember/ThicketMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.thicketmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThicketMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThicketMemberApplication.class, args);
+    }
+
+}
