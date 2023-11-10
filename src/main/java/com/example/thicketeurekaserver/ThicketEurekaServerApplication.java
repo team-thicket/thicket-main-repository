c2fe132d301b25e747d7acc375a5f8548ae84a97
oh jinseok <ojs258@naver.com>
2023-11-10 14:49:59 +0900

    init

---
+++ src/main/java/com/example/thicketeurekaserver/ThicketEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.thicketeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThicketEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThicketEurekaServerApplication.class, args);
+    }
+
+}
