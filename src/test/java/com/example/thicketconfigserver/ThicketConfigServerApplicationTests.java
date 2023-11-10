4aa000a2c48e404645dcd3f5c174571de45c45fc
oh jinseok <ojs258@naver.com>
2023-11-10 14:50:46 +0900

    init

---
+++ src/test/java/com/example/thicketconfigserver/ThicketConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thicketconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThicketConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
