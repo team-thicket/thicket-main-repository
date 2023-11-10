a8d5873d672d7aef470fc65370f8c76765fd6e5f
oh jinseok <ojs258@naver.com>
2023-11-10 14:52:07 +0900

    init

---
+++ src/main/java/com/example/thicketgateway/ThicketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.thicketgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThicketGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThicketGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thicketgateway/ThicketGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thicketgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThicketGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
