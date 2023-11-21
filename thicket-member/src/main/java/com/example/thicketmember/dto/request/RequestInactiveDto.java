737860f2c5359c617096021a1b4eedf258825e57
RunKaRin <dlrjswnrla@naver.com>
2023-11-21 20:13:36 +0900

    2023-11-21, init,requestInactiveDto, conflict해결

--- thicket-member/src/main/java/com/example/thicketmember/dto/request/RequestInactiveDto.java
+++ thicket-member/src/main/java/com/example/thicketmember/dto/request/RequestInactiveDto.java
@@ -1,7 +1,8 @@
 package com.example.thicketmember.dto.request;
 
-import lombok.*;
-
+import com.example.thicketmember.enumerate.MemberStatus;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
 @Data
 public class RequestInactiveDto {
