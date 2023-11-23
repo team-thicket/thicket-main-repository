3607184ce7ed3ab9e08f6cc6713db12b06500f24
RunKaRin <dlrjswnrla@naver.com>
2023-11-23 20:22:13 +0900

    2023-11-23, 포트번호_수정,스웨거설정_제거, 코드컨벤션

--- thicket-chair/src/main/java/com/example/thicketchair/config/SwaggerConfig.java
+++ thicket-chair/src/main/java/com/example/thicketchair/config/SwaggerConfig.java
@@ -5,9 +5,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info( title = "Chair API 명세서",
-                description = "좌석 어플 서비스 API 명세서",
-                    version = "v1"))
+        info = @Info(title = "Chair API 명세서",
+               description = "좌석 어플 서비스 API 명세서",
+                   version = "v1"))
 @Configuration
 public class SwaggerConfig {
 }
