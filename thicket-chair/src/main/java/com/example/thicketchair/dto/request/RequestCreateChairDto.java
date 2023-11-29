2ed854e063be39b0d552e3346dac34bd2044382e
RunKaRin <dlrjswnrla@naver.com>
2023-11-29 16:41:55 +0900

    2023-11-29, 불필요한import제거, 코드컨벤션

--- thicket-chair/src/main/java/com/example/thicketchair/dto/request/RequestCreateChairDto.java
+++ thicket-chair/src/main/java/com/example/thicketchair/dto/request/RequestCreateChairDto.java
@@ -4,7 +4,6 @@ import com.example.thicketchair.domain.Chair;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
