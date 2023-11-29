7804d266b5258952555d83c929addc4f10fbf3f1
RunKaRin <dlrjswnrla@naver.com>
2023-11-29 14:46:01 +0900

    2023-11-29, 유레카설정, 추가구현

--- thicket-chair/src/main/java/com/example/thicketchair/dto/request/RequestCreateChairDto.java
+++ thicket-chair/src/main/java/com/example/thicketchair/dto/request/RequestCreateChairDto.java
@@ -4,6 +4,7 @@ import com.example.thicketchair.domain.Chair;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -19,13 +20,13 @@ public class RequestCreateChairDto {
     @Schema(description = "좌석타입", example = "VIP")
     private String chairType;
 
-    @Min(value = 0, message = "좌석은 음수로 등록할 수 없습니다.")
+    @Min(value = 0, message = "좌석을 음수로 등록할 수 없습니다.")
     @Max(value = 99999, message = "좌석는 99999개 이상 설정할 수 없습니다.")
     @NotNull(message = "좌석 수량을 입력하세요.")
     @Schema(description = "수량", example = "100")
     private int count;
 
-    @Min(value = 0, message = "가격은 음수로 등록할 수 없습니다.")
+    @Min(value = 0, message = "가격을 음수로 등록할 수 없습니다.")
     @NotNull(message = "가격을 입력하세요.")
     @Schema(description = "가격", example = "189000")
     private int price;
