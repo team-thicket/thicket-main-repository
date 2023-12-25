30e9626821d3b740910daa055549e77f80a8d519
RunKaRin <dlrjswnrla@naver.com>
2023-12-25 23:13:38 +0900

    2023-12-25, CSS 일부 복구, 억까당함

--- thicket-front/src/member/component/MemberTicketingList.jsx
+++ thicket-front/src/member/component/MemberTicketingList.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import {Container, H1, Table, Td, Th} from '../../assets/css/setting/admin/StylesOfList';
 
+const tmp = [
+    {
+        "id": "e4e64cbb-cc73-40b4-805e-62be18ab47e4",
+        "createdAt": "2023.09.09.", // 예매일
+        "stageName": "뮤지컬 <마리퀴리>",  // 이름
+        "stagetype": "Musical",
+        "place": "홍익대 대학로 아트센터 대극장",    // 장소
+        "date": "2023.09.09. 09:30", // 오픈시간
+        "cancel": "예매취소",
+        "status": "결제완료"
+    },
+    {
+        "id": "string2",
+        "createdAt": "string2",
+        "stageName": "청소년극 <발가락 육상천재>",
+        "stagetype": "Play",
+        "place": "국립극단 소극장판",
+        "date": "string2",
+        "cancel": "2023.11.09.",
+        "status": "결제대기"
+    },
+    {
+        "id": "string3",
+        "createdAt": "string3",
+        "stageName": "Concert",
+        "stagetype": "Concert",
+        "place": "string3",
+        "date": "string3",
+        "cancel": "string3",
+        "status": "string3"
+    }
+];
+
 export const MemberTicketingList = ({ contentHandler }) => {
     // const handleTitleClick = () => {
     //     contentHandler("클릭 이동방식. AdminBeforeList 참고");
@@ -15,35 +48,27 @@ export const MemberTicketingList = ({ contentHandler }) => {
                 <Table>
                     <tbody>
                     <tr>
-                        <Th width="auto">예매일</Th>
-                        <Th width="auto">티켓고유번호</Th>
-                        <Th width="auto">구분</Th>
+                        <Th width="120px">예매일</Th>
+                        <Th width="115px">티켓고유번호</Th>
+                        <Th width="70px">구분</Th>
                         <Th width="auto">공연명</Th>
-                        <Th width="auto">장소</Th>
-                        <Th width="auto">공연일</Th>
-                        <Th width="auto">예매취소</Th>
-                        <Th width="auto">상태</Th>
+                        <Th width="150px">장소</Th>
+                        <Th width="120px">공연일</Th>
+                        <Th width="120px">취소가능일</Th>
+                        <Th width="85px">상태</Th>
                     </tr>
-                    <tr>
-                        <Td>2023.11.09</Td>
-                        <Td>T12345</Td>
-                        <Td>MUSICAL</Td>
-                        <Td>뮤지컬 &lt;마리퀴리&gt;</Td>
-                        <Td>홍익대 대학로 아트센터 대극장</Td>
-                        <Td>2023.11.25 19:30</Td>
-                        <Td>예매 취소</Td>
-                        <Td>결제 대기</Td>
-                    </tr>
-                    <tr>
-                        <Td>2022.11.03</Td>
-                        <Td>T23456</Td>
-                        <Td>PLAY</Td>
-                        <Td>청소년극 &lt;발가락 육상천재&gt;</Td>
-                        <Td>국립극단 소극장판</Td>
-                        <Td>2022.11.18 19:30</Td>
-                        <Td>2023.11.16 23:59까지</Td>
-                        <Td>예매 완료</Td>
-                    </tr>
+                    {tmp.map((item) => (
+                        <tr key={item.id}>
+                            <Td>{item.createdAt}</Td>
+                            <Td>{item.id.slice(0, 4)}**{item.id.slice(-2)}</Td>
+                            <Td>{item.stagetype}</Td>
+                            <Td>{item.stageName}</Td>
+                            <Td>{item.place}</Td>
+                            <Td>{item.date}</Td>
+                            <Td>{item.cancel}</Td>
+                            <Td>{item.status}</Td>
+                        </tr>
+                    ))}
                     </tbody>
                 </Table>
             </div>
